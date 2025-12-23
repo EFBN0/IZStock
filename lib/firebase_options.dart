000058bd6a9691e4c02eca2da3bd3895a30585c0
Erick Ferreira <erickferreira1319@gmail.com>
2025-12-23 11:05:07 -0300

    feat: adiciona suporte para webapp

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -64,6 +61,15 @@ class DefaultFirebaseOptions {
     projectId: 'iz-stock-app',
     storageBucket: 'iz-stock-app.firebasestorage.app',
     iosBundleId: 'com.example.izstock',
+  );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyCkdzwiOF7Esvuz644lUAdMRO_NJDJkKk8',
+    appId: '1:1016768731650:web:4a5373042a75442493e51b',
+    messagingSenderId: '1016768731650',
+    projectId: 'iz-stock-app',
+    authDomain: 'iz-stock-app.firebaseapp.com',
+    storageBucket: 'iz-stock-app.firebasestorage.app',
   );
 
 }
